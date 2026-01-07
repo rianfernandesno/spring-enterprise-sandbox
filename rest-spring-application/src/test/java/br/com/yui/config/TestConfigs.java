67216d43deec53c4d8d231b2392254adba027356
rianfernandesno <zrpapz12@gmail.com>
2026-01-07 14:47:16 -0300

    "CorsOrigin"

--- rest-spring-application/src/test/java/br/com/yui/config/TestConfigs.java
+++ rest-spring-application/src/test/java/br/com/yui/config/TestConfigs.java
@@ -2,4 +2,10 @@ package br.com.yui.config;
 
 public interface TestConfigs {
     int SERVER_PORT = 8888;
+
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+    String ORIGIN_BLA = "https://www.erudio.com.br";
+    String ORIGIN_GITHUB = "https://github.com/rianfernandesno";
+    String ORIGIN_LOCAL = "http://localhost:8080";
 }
